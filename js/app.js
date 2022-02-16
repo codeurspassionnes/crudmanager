08c6b6b6dd2853856b23b37812b9e516e365ccb5
Codeurs passionnés <>
2022-02-16 13:43:25 +0100

    rendu condionnel

--- js/app.js
+++ js/app.js
@@ -1,9 +1,52 @@
 const App = {
     data() {
         return {
-            message: "Slt les amis yann!"
+            showHome: false,
+            showCreateForm: false,
+            showStudentsList: false,
         }
     },
+
+    mounted(){
+        this.changeNavigationState("home");
+    },
+
+    methods: {
+        goToHome() {
+            this.changeNavigationState("home")
+        },
+        goToCreateForm() {
+            this.changeNavigationState("create")
+        },
+
+        goToStudentsList() {
+            this.changeNavigationState("list")
+        },
+
+        
+
+        changeNavigationState(destination) {
+            this.showCreateForm = false
+            this.showHome = false
+            this.showStudentsList = false
+
+            switch (destination) {
+                case "home":
+                    this.showHome = true
+                    break;
+                case "create":
+                    this.showCreateForm = true
+                    break;
+                case "list":
+                    this.showStudentsList = true
+                    break;
+
+                default:
+                    this.showHome = true
+                    break;
+            }
+        }
+    }
 }
 
 Vue.createApp(App).mount("#app")
